8602247bec0939101f434f12e0f443a1a5713f94
Armaan Soni <itsarmaansoni@gmail.com>
2026-03-27 10:46:16 +0530

    supabase client path

--- src/NewInvoiceForm.jsx
+++ src/NewInvoiceForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../src/supabaseclient';
+import { supabase } from './supabaseClient';
 import '../components/Dashboard.css';
 
 const NewInvoiceForm = ({ onCancel, onSuccess, initialData }) => {
